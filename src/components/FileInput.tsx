d7060ef69bb503a67c67e2206064b9e2c723bff6
Icaro <icarovscarvalho@gmail.com>
2023-12-07 22:35:29 -0300

    Funcionalidade de buscar o documento documento adicionada
    Foi adicionada a feature de buscar documento nos arquivos por meio da lib Dropzone

--- src/components/FileInput.tsx
+++ src/components/FileInput.tsx
@@ -1,10 +1,24 @@
-
+import { useCallback,useState } from "react";
+import {useDropzone} from 'react-dropzone'
 
 function FileInput() {
-    
+    const [file, setFile] = useState <File | null> (null);
+
+    const onDrag = useCallback((files: File[]) => {
+        setFile(files[0])
+    }, []);
+
+    if (file) return null;
+
+    const dropzone = useDropzone({
+        onDrag,
+        accept: {
+            'application/pdf': ['.pdf']
+        }
+    })
 
     return(
-        <div 
+        <div {...dropzone.getRootProps()}
         className="w-1/2 h-96 rounded-lg border-dashed border-2 border-blue-400 hover:border-blue-500 bg-blue-50 hover:bg-blue-100 transition-all">
             <label htmlFor="dropzone-file" className="cursor-pointer w-full h-full">
                 <div className="w-full h-full p-5 flex flex-col items-center justify-center hover:scale-105 transition-all">
@@ -14,7 +28,7 @@ function FileInput() {
                     <p className="text-blue-400 text-sm">PDF</p>
                 </div>
             </label>
-            <input 
+            <input {...dropzone.getInputProps()}
             className="hidden" />
         </div>
     );
