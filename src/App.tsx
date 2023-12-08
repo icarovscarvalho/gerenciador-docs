4417415542262b807b5a046e2ea0a80ad7336c31
Icaro <icarovscarvalho@gmail.com>
2023-12-07 22:30:18 -0300

    Atribuindo Estilização com Tailwind CSS
    Foram adicionados os estilos primários com uso da lib tailwindcss.

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,8 @@ function App() {
   const onSubmit = () => {};
 
   return(
-    <div >
-      <h1 >
+    <div className="bg-gray-100 w-screen h-screen py-10 flex flex-col justify-start items-center gap-y-5">
+      <h1 className="text-4xl text-blue-600 font-bold">
         Input de Arquivos
       </h1>
       <form onSubmit={onSubmit}></form>
--- src/components/FileInput.tsx
+++ src/components/FileInput.tsx
@@ -4,17 +4,18 @@ function FileInput() {
     
 
     return(
-        <div >
-            <label>
-                <div>
-                    <p>
-                        <span>Click Here</span> or drag file!
+        <div 
+        className="w-1/2 h-96 rounded-lg border-dashed border-2 border-blue-400 hover:border-blue-500 bg-blue-50 hover:bg-blue-100 transition-all">
+            <label htmlFor="dropzone-file" className="cursor-pointer w-full h-full">
+                <div className="w-full h-full p-5 flex flex-col items-center justify-center hover:scale-105 transition-all">
+                    <p className="mb-2 text-lg text-blue-600">
+                        <span className="font-bold">Click Here</span> or drag file!
                     </p>
-                    <p >PDF</p>
+                    <p className="text-blue-400 text-sm">PDF</p>
                 </div>
             </label>
             <input 
-             />
+            className="hidden" />
         </div>
     );
 }
